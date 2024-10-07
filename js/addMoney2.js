5452d1c1d584f422f25220d5569bd8cf9a0603fb
RabeyaAS <rabeya.98@hotmail.com>
2024-10-07 17:29:43 +0100

    validated NaN, added to transaction history, cash out denied if its higher than the balance

--- js/addMoney2.js
+++ js/addMoney2.js
@@ -4,11 +4,27 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     const addMoney = getInputFieldValueById2('input-add-money');
     const pinNumber = getInputFieldValueById2('input-pin-number');
 
+    // Validating if its a Not A Number, if so then should return and end there.
+    if(isNaN(addMoney)){
+        alert('Cannot add money.')
+        return;    //we used return so it doesn't move forward and make any changes.
+    }
+
+    // Validating if the pin is correct then do the following - (is it a wrong way to validate pin)
     if(pinNumber === 123){
         const balance = getTextFieldValueById('account-balance');
         const newBalance = balance + addMoney;
         
         document.getElementById('account-balance').innerText = newBalance;
+
+        // add to Transaction History --> the ${} allows to show the amount in transaction.
+        const p = document.createElement('p');
+        p.innerText = `Added: ${addMoney} Tk. New Balance: ${newBalance}`;
+        console.log(p);
+
+        // should be a common function but we are doing this way for now.
+        document.getElementById('transaction-container').appendChild(p);
+
     }
     else{
         alert('Failed to add money!')
--- js/cashOut.js
+++ js/cashOut.js
@@ -6,11 +6,36 @@ document.getElementById('btn-cash-out').addEventListener('click', function(event
     const cashOut = getInputFieldValueById2('input-cash-out');
     const pinNumber = getInputFieldValueById2('input-cash-out-pin');
 
+    // Validating if it is Not A Number
+    if(isNaN(cashOut)){
+        alert('Failed to Cash Out.');
+        return;
+    }
+
+
+    // Validating if the pin is correct then do the following - (is it a wrong way to validate pin)
     if(pinNumber === 123){
         const balance = getTextFieldValueById('account-balance');
+
+        // Stopping and Warning that you are unable to cash out money as you don't have enough balance.
+        if(cashOut > balance){
+            alert('You do not have enough money to cash out');
+            return;
+        }
+
         const newBalance = balance - cashOut;
 
-        document.getElementById('account-balance').innerText = newBalance;   
+        document.getElementById('account-balance').innerText = newBalance;
+        
+        // add to transaction history
+        const div = document.createElement('div');
+        div.classList.add('bg-yellow-300');
+        div.innerHTML = `
+            <h4 class="text-2xl font-bold">Cash Out</h4>
+            <p>${cashOut} Withdraw. New Balance ${newBalance}</p>
+        `
+        
+        document.getElementById('transaction-container').appendChild(div);
     }
     else{
         alert('Wrong Pin entered. Try again later.')
